2745f22d6ba143ed4f394ca3615765b8c9d3554c
0918nobita <nobita.0918@gmail.com>
2019-02-19 22:52:08 +0900

    body-parser を導入した

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const channelAccessToken = 'xJDP5i5HkrbQjpMSuKErT0Djyp/r+OQxjjaNJ/H+bFbEP6esa/1bcKJr2eNilMeCy7jve90C0Pxf1CZrsoGvTigkOWzzJdAMuCyiWcDAyqtP0yk3467SAv+JXGQt3cgXUu8Dey2I0tZumMqeZ/TFdgdB04t89/1O/w1cDnyilFU=';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios');
+
+app.use(bodyParser.json());
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Node.js is listening to PORT: ${server.address().port}`);
