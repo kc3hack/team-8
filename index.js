1c0506bf718bfb62004c2adb0d2c125e188ae9ba
0918nobita <nobita.0918@gmail.com>
2019-02-20 00:16:53 +0900

    :sparkles: カルーセルテンプレートのサンプルを追加した

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const channelAccessToken = 'xJDP5i5HkrbQjpMSuKErT0Djyp/r+OQxjjaNJ/H+bFbEP6esa/1bcKJr2eNilMeCy7jve90C0Pxf1CZrsoGvTigkOWzzJdAMuCyiWcDAyqtP0yk3467SAv+JXGQt3cgXUu8Dey2I0tZumMqeZ/TFdgdB04t89/1O/w1cDnyilFU=';
+const url = 'https://api.line.me/v2/bot/message/reply';
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const axios = require('axios');
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -18,22 +19,77 @@ app.get('/', (req, res, next) => {
 app.post('/webhook', function (req, res, next) {
   res.status(200).end();
 
-  console.log(req);
   for (let event of req.body.events) {
-    if (event.type === 'message' && event.message.text === 'ハロー') {
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${channelAccessToken}`
-      };
-      const body = {
-        replyToken: event.replyToken,
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${channelAccessToken}`
+    };
+
+    if (event.type === 'message') {
+      if (event.message.text === 'ハロー') {
+        axios.post(url, {
+          replyToken: event.replyToken,
+          messages: [{
+            type: 'text',
+            text: 'こんにちは'
+          }]
+        }, { headers });
+      } else if (event.message.text === 'テスト') {
+        axios.post(url, {
+          replyToken: event.replyToken,
+          messages: [{
+            type: 'template',
+            altText: 'これはテンプレートメッセージです。このバージョンでは対応していません。',
+            template: [{
+              type: 'carousel',
+              columns: [
+                {
+                  thumbnailImageUrl: 'https://res.cloudinary.com/tsundoku/image/upload/v1550587379/p_002.jpg',
+                  title: '白咲 花',
+                  text: 'しろさき はな',
+                  actions: [{
+                    type: 'postback',
+                    label: 'かわいい',
+                    data: 'hana'
+                  }]
+                },
+                {
+                  thumbnailImageUrl: 'https://res.cloudinary.com/tsundoku/image/upload/v1550587383/p_003.jpg',
+                  title: '星野 ひなた',
+                  text: 'ほしの ひなた',
+                  actions: [{
+                    type: 'postback',
+                    label: 'かわいい',
+                    data: 'hinata'
+                  }]
+                },
+                {
+                  thumbnailImageUrl: 'https://res.cloudinary.com/tsundoku/image/upload/v1550587388/p_004.jpg',
+                  title: '姫坂 乃愛',
+                  text: 'ひめさか のあ',
+                  actions: [{
+                    type: 'postback',
+                    label: 'かわいい',
+                    data: 'noa'
+                  }]
+                }
+              ]
+            }]
+          }]
+        }, { headers });
+      }
+    } else if (event.type === 'postback') {
+      axios.post(url, {
+        replyToken: event.body.replyToken,
         messages: [{
           type: 'text',
-          text: 'こんにちは'
+          text: event.postback.data === 'hana'
+              ? '花「ありがとうございます。」'
+              : (event.postback.data === 'hinata'
+                  ? 'ひなた「やったぁー！」'
+                  : '乃愛「でっしょー？私が世界で一番かわいい！」')
         }]
-      };
-      const url = 'https://api.line.me/v2/bot/message/reply';
-      axios.post(url, body, { headers });
+      }, { headers });
     }
   }
-});
+);
