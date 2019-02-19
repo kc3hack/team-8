566e32826a0b7ba7f3d1e9e60bff3eabda680408
0918nobita <nobita.0918@gmail.com>
2019-02-19 21:57:30 +0900

    :sparkles: 簡易的な LINE Bot を実装した

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+const channelAccessToken = 'xJDP5i5HkrbQjpMSuKErT0Djyp/r+OQxjjaNJ/H+bFbEP6esa/1bcKJr2eNilMeCy7jve90C0Pxf1CZrsoGvTigkOWzzJdAMuCyiWcDAyqtP0yk3467SAv+JXGQt3cgXUu8Dey2I0tZumMqeZ/TFdgdB04t89/1O/w1cDnyilFU=';
+
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Node.js is listening to PORT: ${server.address().port}`);
 });
 
 app.get('/', (req, res, next) => {
   res.json('Hello World!');
+});
+
+app.post('/webhook', (req, res, next) => {
+  res.status(200).end();
+
+  for (let event of req.body.events) {
+    if (event.type === 'message' && event.message.text === 'ハロー') {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${channelAccessToken}`
+      };
+      const body = {
+        replyToken: event.replyToken,
+        messages: [{
+          type: 'text',
+          text: 'こんにちは'
+        }]
+      };
+      const url = 'https://api.line.me/v2/bot/message/reply';
+      axios.post(url, body, { headers });
+    }
+  }
 });
